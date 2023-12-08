05465642fd589394022ba0ecc42f72ba4af20933
Neelam <neelammujawar12@gmail.com>
2023-12-08 11:55:48 +0530

    check equality of 2 numbers

--- LineComparision.java
+++ LineComparision.java
@@ -12,10 +12,30 @@ public class LineComparision {
         double x2 = sc.nextDouble();
         System.out.print("Enter y2: ");
         double y2 = sc.nextDouble();
-        double deltaX = x2 - x1;
-        double deltaY = y2 - y1;
-        double length = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-        System.out.println("The length of the line is: " + length);
-    
+        double firstLineDeltaX = firstLine_x2 - firstLine_x1;
+        double firstLineDeltaY = firstLine_y2 - firstLine_y1;
+        double firstLineLength = Math.sqrt(firstLineDeltaX * firstLineDeltaX + firstLineDeltaY * firstLineDeltaY);
+        System.out.println("The length of the line is: " + firstLineLength);
+        System.out.println("Enter second line details : ");
+        System.out.print("Enter x1: ");
+        double secondLine_x1 = sc.nextDouble();
+        System.out.print("Enter y1: ");
+        double secondLine_y1 = sc.nextDouble();
+        System.out.print("Enter x2: ");
+        double secondLine_x2 = sc.nextDouble();
+        System.out.print("Enter y2: ");
+        double secondLine_y2 = sc.nextDouble();
+        double secondLineDeltaX = secondLine_x2 - secondLine_x1;
+        double secondLineDeltaY = secondLine_y2 - secondLine_y1;
+        double secondLineLength = Math.sqrt(secondLineDeltaX * secondLineDeltaX + secondLineDeltaY * secondLineDeltaY);
+       
+        System.out.println("The length of the line is: " + secondLineLength);
+        if(firstLineLength == secondLineLength) {
+            System.out.println("Both lines have equal length");
+        }
+        else
+        {
+            System.out.println("Lines are not equal");
+        }
     }
-}
+    }
