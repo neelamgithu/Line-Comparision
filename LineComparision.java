e1376def4ddd10845595cbc7e5061594e2e5815b
Neelam <neelammujawar12@gmail.com>
2023-12-08 12:00:43 +0530

    check wether two lines are equal greater than or smaller

--- LineComparision.java
+++ LineComparision.java
@@ -29,13 +29,16 @@ public class LineComparision {
         double secondLineDeltaY = secondLine_y2 - secondLine_y1;
         double secondLineLength = Math.sqrt(secondLineDeltaX * secondLineDeltaX + secondLineDeltaY * secondLineDeltaY);
        
-        System.out.println("The length of the line is: " + secondLineLength);
-        if(firstLineLength == secondLineLength) {
-            System.out.println("Both lines have equal length");
+        String firstLineLengthString = Double.toString(firstLineLength);
+        String secondLineLengthString = Double.toString(secondLineLength);
+        if(firstLineLengthString.compareTo(secondLineLengthString)==0) {
+            System.out.println("Both lines have same length");
         }
-        else
-        {
-            System.out.println("Lines are not equal");
+        else if(firstLineLengthString.compareTo(secondLineLengthString)>0) {
+            System.out.println("First line is greater than second line");
+        }
+        else {
+            System.out.println("First line is smaller than second line");
         }
     }
     }
